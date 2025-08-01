1d1aa2f7f6dfbd961e12b4dc45a9653efc43caca
vitallygolovanov <89348452+vitallygolovanov@users.noreply.github.com>
2025-08-01 19:41:04 +0300

    chore: update dependencies to latest versions for improved performance and security

--- .babelrc.js
+++ .babelrc.js
@@ -9,10 +9,7 @@ module.exports = {
     ],
   ],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-object-rest-spread',
     [
       '@babel/plugin-transform-runtime',
       {
